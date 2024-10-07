08eca8b212be94806dc5e6c602a80d82293015d8
belly1y <165050412+belly1y@users.noreply.github.com>
2024-10-07 11:33:05 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'História,
+    'Qual foi o último continente a ser povoado pelos seres humanos?'
+    'Oceania'
+)
+criaCartao(
+    'Artes',
+    'quais são as cores primarias'
+    'amarelo, o azul e o vermelho'
+)
+criaCartao(
+    'quimica',
+    'oque é uma molécula',
+    'é um conjunto de átomos ligados quimicamente, formando a menor unidade de uma substância que mantém suas propriedades químicas.'
+)
+criaCartao(
+    'Educação fisica',
+    'Quando surgiu a Olimpíada?
+    'As primeiras Olimpíadas remontam a 776 a.C.'
+)
+criaCartao(
+    'geografia',
+    'Como definir o relevo?'
+    'é o conjunto de feições da superfície terrestre'
+)
+criaCartao(
+    'Historia',
+    'quem descobriu o Brasil?'
+    'Pedro Àlves Cabral'
